002ced6074bd8607864768433eb3d6dc23922943
Sohail Pathan <apyhub@Sohails-Macbook-Air.local>
2024-02-09 20:49:20 +0530

    csv file changes

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,10 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+
+
 export default {
+
 	async fetch(request: Request, env: any, ctx: any): Promise<Response> {
 		if (request.method !== 'POST') {
             return new Response('Expected POST', { status: 405 });
@@ -31,13 +34,16 @@ export default {
             const batch = emails.slice(i, i + 10);
             const batchResults = await Promise.all(batch.map(email => this.validateEmail(email)));
 
-            batch.forEach((email, index) => {
-                if (batchResults[index]) {
-                    validEmails.push(email);
-                } else {
-                    invalidEmails.push(email);
-                }
-            });
+			// console.log(batchResults)
+
+            batchResults.forEach((email: any, index) => {
+				if (email.data) {
+					 validEmails.push(batch[index]) 
+				}
+                else {
+					invalidEmails.push(batch[index])
+				}
+			} );
 
             // Wait for 1 second before processing the next batch, but skip for the last batch
             if (i + 10 < emails.length) {
@@ -45,14 +51,17 @@ export default {
             }
         }
 
-        const output = `validEmails,invalidEmails\n${validEmails.join(',')},${invalidEmails.join(',')}`;
+		let csvContent = 'Emails,Invalid Emails\n'; emails.forEach((email, index) => {csvContent += `${email},${invalidEmails[index]}\n`;
+});
 
-        return new Response(output, {
+        return new Response(csvContent.replaceAll('undefined', ''), {
             headers: {
                 'Content-Type': 'text/csv',
                 'Content-Disposition': 'attachment; filename="email_validation_result.csv"'
             }
         });
+
+	
 	},
 
 	async validateEmail(email: string): Promise<boolean> {
@@ -62,16 +71,14 @@ export default {
                 'Content-Type': 'application/json',
                 'apy-token': 'APY0fp0wB7eK6PgABfXjXkiYb5jFyBusSJGpUinCDf2kmyNRO7w5YpwDUUbYJ6agMXc'
             },
-            body: JSON.stringify({ email })
+			
+			body: JSON.stringify({email})
         });
-
-		console.log(apiResponse);
-
+	
         if (!apiResponse.ok) {
-            console.error(`API call failed with status: ${apiResponse.status}`);
+            console.error(`API call failed with status: 400`);
             return false;
         }
-
-        return await apiResponse.json();
+        return await apiResponse.json()
     },
 };
